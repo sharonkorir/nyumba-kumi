a4b0a9e35e56f697b1b39925a2aa35fb6b59ef64
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-18 02:43:06 +0300

    user auth: add welcome email feature

--- users/views.py
+++ users/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .models import Newsletter
+
+from neighbourhood.models import Profile
+from users.email import send_welcome_email
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form':form})
+    '''
+    Register a new user on registration and create user profile using signals
+    '''
+    if request.method == 'POST':
+      form = UserRegistrationForm(request.POST)
+      if form.is_valid():
+        form.save()
+        #username = form.cleaned_data.get('username')
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        #welcome email
+        recipient = Newsletter(name = username,email =email)
+        recipient.save()
+        send_welcome_email(username,email)
+        #succesful log in message
+        messages.success(request, f'Your nyumba kumi account had been created successfully')
+        
+        return redirect('login')
+    else:
+      form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+    
