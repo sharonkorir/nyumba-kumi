89c535e371b70c3371b2cf2778027e0dac71990d
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-18 00:47:18 +0300

    add neighbourhood class

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    #logo = CloudinaryField('image')
+    #admin = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    health_dept = models.IntegerField(null=True, blank=True)
+    police_dept = models.IntegerField(null=True, blank=True)
+
+
