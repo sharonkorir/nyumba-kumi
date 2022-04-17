9463c40124a4fede4492b5658b7c3f2c27be09eb
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-18 01:40:09 +0300

    create business class, methods and tests

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -33,9 +33,36 @@ class Neighbourhood(models.Model):
 class Profile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=60, blank=True)
-    bio = models.TextField(max_length=254, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
     profile_photo = CloudinaryField('image')
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=60)
+    logo = CloudinaryField('image')
+    description = models.TextField(max_length=200, blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+      return self.name
+
+    def create_busn(self):
+        self.save()
+
+    @classmethod
+    def delete_busn(cls, pk):
+        cls.objects.filter(pk=pk).delete()
+
+    @classmethod
+    def find_busn(cls, id):
+        search_results = cls.objects.filter(id=id)
+        return search_results
+
+    def update_busn(self, name):
+      self.name = name
+      self.save()
--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,31 +1,55 @@
 from django.test import TestCase
-from .models import Neighbourhood
+from .models import Neighbourhood, Business
 
 # Create your tests here.
 class NeighbourhoodTestClass(TestCase):
-  def setUp(self):
-        self.new_neighbourhood = Neighbourhood(id= 1, name='west', location='langata', health_dept= '911', police_dept= '911', logo= 'random.jpg')
+    def setUp(self):
+          self.new_neighbourhood = Neighbourhood( name='west', location='langata', health_dept= '911', police_dept= '911', logo= 'random.jpg')
 
-  def test_instance(self):
-      self.assertTrue(isinstance(self.new_neighbourhood, Neighbourhood))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_neighbourhood, Neighbourhood))
 
-  def tearDown(self):
-     Neighbourhood.objects.filter(id=1).delete()
+    def tearDown(self):
+      Neighbourhood.objects.filter(id=1).delete()
 
-  def test_create_neighbourhd(self):
-      self.new_neighbourhood.create_neighbourhd()
-      self.assertTrue(len(Neighbourhood.objects.all())>0)
+    def test_create_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
+        self.assertTrue(len(Neighbourhood.objects.all())>0)
 
-  def test_delete_neighbourhd(self):
-      self.new_neighbourhood.delete_neighbourhd(1)
-      self.assertTrue(len(Neighbourhood.objects.all()) == 0)
+    def test_delete_neighbourhd(self):
+        self.new_neighbourhood.delete_neighbourhd(1)
+        self.assertTrue(len(Neighbourhood.objects.all()) == 0)
 
-  def test_find_neighbourhd(self):
-      self.new_neighbourhood.create_neighbourhd()
-      self.assertTrue(len(Neighbourhood.objects.filter(id=1)) == 1)
+    def test_find_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
+        self.assertTrue(len(Neighbourhood.objects.filter(id=1)) == 1)
 
-  def test_update_neighbourhd(self):
-      self.new_neighbourhood.create_neighbourhd()
-      pass
+    def test_update_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
+        pass
 
-    
+class BusinessTestClass(TestCase):
+    def setUp(self):
+          self.new_business = Business( name='smokie pasua', description='street food', Neighbourhood=1, owner= 1, email= 'smokiepasua@gmail.com', logo= 'random.jpg')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_business, Business))
+
+    def tearDown(self):
+        Business.objects.filter(id=1).delete()
+
+    def test_create_business(self):
+        self.new_business.create_busn()
+        self.assertTrue(len(Business.objects.all())>0)
+
+    def test_delete_business(self):
+        self.new_business.delete_busn(1)
+        self.assertTrue(len(Business.objects.all()) == 0)
+
+    def test_find_business(self):
+        self.new_business.create_busn()
+        self.assertTrue(len(Business.objects.filter(id=1)) == 1)
+
+    def test_update_business(self):
+        pass
+      
